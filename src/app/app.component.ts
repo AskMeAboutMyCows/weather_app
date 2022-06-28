5adbd223e483ab4c72a4f38870e45d1d154f091b
Hunter <fffarmer@protonmail.com>
2022-06-28 19:47:26 -0400

    added logic to app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather_app';
+export class AppComponent implements OnInit, OnDestroy {
+  cities = ["London", "Paris", "Moscow", "New York", "Karachi", "Sydney"];
+
+  cityControl: FormControl;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+  }
+
+  ngOnDestroy() {
+  }
 }
