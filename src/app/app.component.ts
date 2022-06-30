a7d16e25b82af00e1bf4565cb85ed730f939b1cd
Hunter <fffarmer@protonmail.com>
2022-06-30 18:13:36 -0400

    added weather api ijectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+
+/// the ingectable below may need to be moved
+
+@Injectable({ providedIn: "root" })
+export class WeatherService {
+  constructor(private http: HttpClient) {}
+
+  getWeatherForCity(city: string): Observable<any> {
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=695ed9f29c4599b7544d0db5c211d499`;
+    return this.http.get(path);
+  }
+}
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
